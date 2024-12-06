77f9eff44da547e14fd8e2e6f7918b3104e44ba4
julk4kulk4 <julk4kulk4@gmail.com>
2024-12-06 13:43:37 +0100

    wykrycie dziur o zadanej powierzchni

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 #import rasterio
 from funcs import read_spatial_raster, segment_image_with_mask, get_band
+import geopandas as gpd
+from shapely.geometry import Polygon
 
 
 
@@ -33,10 +35,29 @@ print(f"number of detected forests: {len(detected_forests)}")
 
 big_forests = detected_forests[detected_forests["geometry"].area > 1000].copy()
 big_forests["id"] = np.int64(big_forests.index)
-print(f"number of detected forests with area > 1000: {len(big_forests)}")
-big_forests.plot(column="id",color="red")
-plt.savefig("big_forests.png")
-plt.show()
+# print(f"number of detected forests with area > 1000: {len(big_forests)}")
+# big_forests.plot(column="id",color="red")
+# plt.savefig("big_forests.png")
+# plt.show()
 
 big_forests.set_crs(epsg=32634, inplace=True)
-big_forests.to_file("big_forests.shp", driver="ESRI Shapefile") #ląduje w afryce
+# big_forests.to_file("big_forests.shp", driver="ESRI Shapefile") #ląduje w afryce
+
+
+
+# Create a GeoDataFrame to store holes
+holes = []
+
+for idx, row in big_forests.iterrows():
+    geometry = row["geometry"]
+    for hole in geometry.interiors:
+        holes.append({"id": row["id"], "geometry": Polygon(hole)})
+
+holes_gdf = gpd.GeoDataFrame(holes, crs=big_forests.crs)
+#holes_gdf = holes_gdf.to_crs({'proj':'cea'}) #odwzorowanie równopolowe
+holes_gdf["area"] = holes_gdf["geometry"].area
+print(f"number of holes: {len(holes_gdf)}")
+holes_gdf = holes_gdf[holes_gdf["area"] > 100].copy()
+print(f"number of holes with area > 100: {len(holes_gdf)}")
+holes_gdf.to_file("forest_holes.shp", driver="ESRI Shapefile")
+

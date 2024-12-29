2fc5d4f20d10433e5a41318b93a4d07f07f090a1
julk4kulk4 <julk4kulk4@gmail.com>
2024-12-29 15:27:44 +0100

    wspólny crs dla zrębów i rastra

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #import rasterio
-from funcs import read_spatial_raster, segment_image_with_mask, get_band
+from funcs import read_spatial_raster, segment_image_with_mask, get_band, reproject_geodataframe, convert_to_pixel_system
 import geopandas as gpd
 from shapely.geometry import Polygon
 
@@ -10,6 +10,8 @@ from shapely.geometry import Polygon
 
 raster_file = r"grupa_4.tif"
 raster_dataset = read_spatial_raster(raster_file)
+raster_projection=raster_dataset.GetProjection()
+#print(raster_projection)
 
 coastal_blue = get_band(raster_dataset, 1)
 blue = get_band(raster_dataset, 2)
@@ -20,44 +22,31 @@ red = get_band(raster_dataset, 6)
 rededge = get_band(raster_dataset, 7)
 nir = get_band(raster_dataset, 8)
 
-#wyszukanie obszarów z drzewami
-# red_array[(red_array > 300)] = 0
-# red_array[np.isnan(red_array)] = 0
-# plt.imshow(red_array, cmap='RdYlGn')
-# plt.show()
-
-forest_mask = (red < 300) & (red > 0)
-forest_mask = np.flipud(forest_mask)
+forest_mask = (red < 300) & (red > 0)   #lasy przyjmuja wartosci od 0 do 300
 forest_mask = np.uint8(forest_mask)
 
 detected_forests =  segment_image_with_mask(forest_mask, forest_mask)
-print(f"number of detected forests: {len(detected_forests)}")
+#print(f"number of detected forests: {len(detected_forests)}")
 
 big_forests = detected_forests[detected_forests["geometry"].area > 1000].copy()
 big_forests["id"] = np.int64(big_forests.index)
-# print(f"number of detected forests with area > 1000: {len(big_forests)}")
-# big_forests.plot(column="id",color="red")
-# plt.savefig("big_forests.png")
-# plt.show()
 
-big_forests.set_crs(epsg=32634, inplace=True)
-# big_forests.to_file("big_forests.shp", driver="ESRI Shapefile") #ląduje w afryce
-
-
-
-# Create a GeoDataFrame to store holes
 holes = []
-
 for idx, row in big_forests.iterrows():
     geometry = row["geometry"]
     for hole in geometry.interiors:
         holes.append({"id": row["id"], "geometry": Polygon(hole)})
 
-holes_gdf = gpd.GeoDataFrame(holes, crs=big_forests.crs)
-#holes_gdf = holes_gdf.to_crs({'proj':'cea'}) #odwzorowanie równopolowe
+holes_gdf = gpd.GeoDataFrame(holes)
 holes_gdf["area"] = holes_gdf["geometry"].area
-print(f"number of holes: {len(holes_gdf)}")
+# print(f"number of holes: {len(holes_gdf)}")
 holes_gdf = holes_gdf[holes_gdf["area"] > 100].copy()
-print(f"number of holes with area > 100: {len(holes_gdf)}")
-holes_gdf.to_file("forest_holes.shp", driver="ESRI Shapefile")
+#print(f"number of holes with area > 100: {len(holes_gdf)}")
+
+holes_gdf.crs = raster_projection
+holes_features = convert_to_pixel_system(holes_gdf, raster_dataset.GetGeoTransform())
+holes_features.to_file("forest_holes.shp", driver="ESRI Shapefile")
+
+
+
 
